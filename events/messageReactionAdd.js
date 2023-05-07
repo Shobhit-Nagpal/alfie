770dc2e0fc4c45b20b41c994e3bfd8237c482579
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-05-07 16:59:08 +0530

    Added title and image in new embed from original embed + Removed img folder

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,69 +3,77 @@ const { EmbedBuilder } = require('discord.js');
 const getThumbnailURLFromMessage = require('../thumbnail');
 
 module.exports = {
-    name: Events.MessageReactionAdd,
-    async execute(reaction, user) {
-        const { message } = reaction;
-        const { guild } = message;
+  name: Events.MessageReactionAdd,
+  async execute(reaction, user) {
+    const { message } = reaction;
+    const { guild } = message;
 
-        if (user.bot) return; // Ignore bot reactions
+    if (user.bot) return; // Ignore bot reactions
 
-        if (!reaction.emoji.id === '1103739159664926741' || !reaction.emoji.name === '⭐') return;
+    if (!reaction.emoji.id === '1103739159664926741' || !reaction.emoji.name === '⭐') return;
 
-        if (reaction.emoji.id === '1103739159664926741') {
-            const sameReactionsBookmark = message.reactions.cache.filter(r => r.emoji.id === '1103739159664926741');
-            if (sameReactionsBookmark.first().count > 1) return;
+    if (reaction.emoji.id === '1103739159664926741') {
+      const sameReactionsBookmark = message.reactions.cache.filter(r => r.emoji.id === '1103739159664926741');
+      if (sameReactionsBookmark.first().count > 1) return;
 
-            // Check if the message has a link
-            if (!message.content.match(/https?:\/\/\S+/)) return;
+      // Check if the message has a link
+      if (!message.content.match(/https?:\/\/\S+/)) return;
 
-            let thumbnail = getThumbnailURLFromMessage(message.content);
-            if(thumbnail === null) {
-                thumbnail = 'https://funnydogsgallery.com/wp-content/uploads/2016/02/smile-dog.jpg';
-            }
+      let thumbnail = getThumbnailURLFromMessage(message.content);
+      if (thumbnail === null) {
+        thumbnail = 'https://funnydogsgallery.com/wp-content/uploads/2016/02/smile-dog.jpg';
+      }
 
-            const contentChannel = guild.channels.cache.find(channel => channel.name === 'content');
+      const contentChannel = guild.channels.cache.find(channel => channel.name === 'content');
 
-            if (!contentChannel) {
-                console.log('Content channel not found. Please create a channel named "content".');
-                return;
-            }
+      if (!contentChannel) {
+        console.log('Content channel not found. Please create a channel named "content".');
+        return;
+      }
 
-            const embed = new EmbedBuilder()
+      const originalEmbed = message.embeds[0];
+
+      if (!originalEmbed) {
+        console.log('No embed found in the message.');
+        return;
+      }
+
+       const embed = new EmbedBuilder()
                 .setColor(0x0099FF)
                 .setAuthor({name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}`})
+                .setTitle(originalEmbed.title)
+                .setURL(originalEmbed.url)
                 .setDescription(`${message.content}`)
                 .setTimestamp()
-                .setImage(thumbnail);
+                .setImage(originalEmbed.thumbnail.url);
+
+      await contentChannel.send({ embeds: [embed] });
+    }
+    else if (reaction.emoji.name === '⭐') {
+      const sameReactions = message.reactions.cache.filter(r => r.emoji.name === '⭐');
 
 
-            await contentChannel.send({ embeds: [embed] });
-        }
-        else if  (reaction.emoji.name === '⭐') {
-            const sameReactions = message.reactions.cache.filter(r => r.emoji.name === '⭐');
 
-            
+      if (sameReactions.first().count < 3) return;
 
-            if (sameReactions.first().count < 3) return;
+      const starboardChannel = guild.channels.cache.find(channel => channel.name === 'starboard');
 
-            const starboardChannel = guild.channels.cache.find(channel => channel.name === 'starboard');
-
-            if (!starboardChannel) {
-                console.log('Content channel not found. Please create a channel named "starboard".');
-                return;
-            }
+      if (!starboardChannel) {
+        console.log('Content channel not found. Please create a channel named "starboard".');
+        return;
+      }
 
 
-            const embed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle('Go to message!')
-                .setURL(`${message.url}`)
-                .setAuthor({name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}`})
-                .setDescription(`${message.content}`)
-                .setTimestamp();
+      const embed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Go to message!')
+        .setURL(`${message.url}`)
+        .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
+        .setDescription(`${message.content}`)
+        .setTimestamp();
 
 
-            await starboardChannel.send({ embeds: [embed] });
-        }
-    },
+      await starboardChannel.send({ embeds: [embed] });
+    }
+  },
 };
