fb35bc58540ba042b3d679de488f41f830d2afb3
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-05-14 12:12:01 +0530

    Changed embed details of recent command

--- commands/recent.js
+++ commands/recent.js
@@ -2,33 +2,34 @@ const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('recent')
-        .setDescription('Returns 3 most recent messages from content channel'),
-    async execute(interaction) {
-        const contentChannel = interaction.guild.channels.cache.find(channel => channel.name === 'content');
+  data: new SlashCommandBuilder()
+    .setName('recent')
+    .setDescription('Returns 3 most recent messages from content channel'),
+  async execute(interaction) {
+    const contentChannel = interaction.guild.channels.cache.find(channel => channel.name === 'content');
 
-        if (!contentChannel) {
-            await interaction.reply('Content channel not found. Please create a channel named "content".');
-            return;
-        }
+    if (!contentChannel) {
+      await interaction.reply('Content channel not found. Please create a channel named "content".');
+      return;
+    }
 
-        const messages = await contentChannel.messages.fetch({ limit: 3 });
+    const messages = await contentChannel.messages.fetch({ limit: 3 });
 
-        if (!messages.size) {
-            await interaction.reply('No messages found in the content channel.');
-            return;
-        }
+    if (!messages.size) {
+      await interaction.reply('No messages found in the content channel.');
+      return;
+    }
 
-        const embeds = messages.map(msg => new EmbedBuilder()
-            .setTitle(`By: ${msg.embeds[0].author.name}`)
-            .setDescription(msg.embeds[0].description)
-            .setColor(0x0099ff)
-            .addFields(
-                {name: 'Go to embed', value: `${msg.url}`}
-            )
-        );
 
-        await interaction.reply({ embeds });
-    },
+    const embeds = messages.map(msg => new EmbedBuilder()
+      .setTitle(`${msg.embeds[0].title}`)
+      .setURL(`${msg.embeds[0].url}`)
+      .setAuthor({ name: `${msg.embeds[0].author.name}`, iconURL: `${msg.embeds[0].author.iconURL}`})
+      .setDescription(`${msg.embeds[0].description}\n\n[Go to embed](${msg.url})`)
+      .setColor(0x0099ff)
+      .setImage(msg.embeds[0].image.url)
+    );
+
+    await interaction.reply({ embeds });
+  },
 };
