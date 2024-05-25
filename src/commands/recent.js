36f3abb0a2c80e798544e4b86195b5291e4c6d81
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-05-25 15:01:30 +0530

    add checks for recent command

--- src/commands/recent.js
+++ src/commands/recent.js
@@ -6,29 +6,53 @@ module.exports = {
     .setName('recent')
     .setDescription('Returns 3 most recent messages from content channel'),
   async execute(interaction) {
-    const contentChannel = interaction.guild.channels.cache.find(channel => channel.name === 'content');
+    const contentChannel = interaction.guild.channels.cache.find(channel => channel.name === "village-library");
 
     if (!contentChannel) {
-      await interaction.reply('Content channel not found. Please create a channel named "content".');
+      await interaction.reply('village-library not found. Please add in the correct name of the channel or create one if it does not exist');
       return;
     }
 
     const messages = await contentChannel.messages.fetch({ limit: 3 });
 
     if (!messages.size) {
-      await interaction.reply('No messages found in the content channel.');
+      await interaction.reply('No messages found in village-library');
       return;
     }
 
+    const embeds = messages.map(msg => {
+      const embedData = msg.embeds[0];
+      const embed = new EmbedBuilder().setColor(0x0099ff);
 
-    const embeds = messages.map(msg => new EmbedBuilder()
-      .setTitle(`${msg.embeds[0].title}`)
-      .setURL(`${msg.embeds[0].url}`)
-      .setAuthor({ name: `${msg.embeds[0].author.name}`, iconURL: `${msg.embeds[0].author.iconURL}`})
-      .setDescription(`${msg.embeds[0].description}\n\n[Go to embed](${msg.url})`)
-      .setColor(0x0099ff)
-      .setImage(msg.embeds[0].image.url)
-    );
+      if (embedData) {
+        if (embedData.title) {
+          embed.setTitle(embedData.title);
+        }
+
+        if (embedData.url && typeof embedData.url === 'string' && embedData.url !== 'null') {
+          embed.setURL(embedData.url);
+        }
+
+        if (embedData.author) {
+          embed.setAuthor({
+            name: embedData.author.name || 'Unknown Author',
+            iconURL: embedData.author.iconURL || null
+          });
+        }
+
+        if (embedData.description) {
+          embed.setDescription(`${embedData.description}\n\n[Go to embed](${msg.url})`);
+        }
+
+        if (embedData.image && embedData.image.url) {
+          embed.setImage(embedData.image.url);
+        }
+      } else {
+        embed.setDescription(`[Go to message](${msg.url})`);
+      }
+
+      return embed;
+    });
 
     await interaction.reply({ embeds });
   },
